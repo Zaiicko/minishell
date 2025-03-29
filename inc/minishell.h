9282bda0d1acfc80229bfa122e8423d8f3941dad
Mehdi <zaiicko7@proton.me>
2025-03-30 00:26:04 +0100

    Add : Makefile, inc & src folder, libft, main with readline, .h)

---
+++ inc/minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nicleena <nicleena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 17:14:40 by nicleena          #+#    #+#             */
+/*   Updated: 2025/03/30 00:11:50 by zaiicko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+// typedef struct t_data;
+
+// struct s_data
+// {
+//     int id;    
+// };
+
+
+
+
+#endif
