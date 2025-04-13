1d7cc5d2a0902d958b889240a7ba82cbaf2a6e2b
Mehdi <zaiicko7@proton.me>
2025-04-13 23:31:17 +0200

    Add : utils for the linked_list of tokens

---
+++ src/lexer_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 19:02:27 by zaiicko           #+#    #+#             */
+/*   Updated: 2025/04/13 23:30:12 by zaiicko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+t_token	*new_token(t_token_type type, char *value)
+{
+	t_token	*token;
+
+	token = (t_token *)malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->type = type;
+	token->value = ft_strdup(value);
+	if (!token->value)
+	{
+		free(token);
+		return (NULL);
+	}
+	token->next = NULL;
+	return (token);
+}
+
+void	add_token_to_list(t_token **head, t_token *new_token)
+{
+	t_token	*current;
+
+	if (!*head)
+		*head = new_token;
+	else
+	{
+		current = *head;
+		while (current->next)
+			current = current->next;
+		current->next = new_token;
+	}
+}
+
+void	free_token_list(t_token **head)
+{
+	t_token	*current;
+	t_token	*next;
+
+	current = *head;
+	while (current)
+	{
+		next = current->next;
+		free_token(current);
+		current = next;
+	}
+	*head = NULL;
+}
+
+void	free_token(t_token *token)
+{
+	if (token)
+	{
+		free(token->value);
+		free(token);
+	}
+}
