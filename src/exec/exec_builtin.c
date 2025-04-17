95229ebbc0699ac4c8edc3810028d020ef6aa3e0
Nicolas Leenaerts <nicleena@mi-c2-p2.s19.be>
2025-04-17 16:22:37 +0200

    Version corrigee de l'exec_builtin

--- src/exec/exec_builtin.c
+++ src/exec/exec_builtin.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec.c                                             :+:      :+:    :+:   */
+/*   exec_builtin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nicleena <nicleena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 15:31:22 by nicleena          #+#    #+#             */
-/*   Updated: 2025/04/17 15:32:36 by nicleena         ###   ########.fr       */
+/*   Updated: 2025/04/17 16:22:20 by nicleena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,28 @@ int is_builtin(char *cmd)
         !strcmp(cmd, "exit")
     );
 }
+void handle_cd(char **args, t_env *env)
+{
+    char *target;
 
-int exec_builtin(char **args, t_env *env)
+    target = NULL;
+    if (args[1])
+        target = args[1];
+    else
+        target = getenv("HOME");
+    ft_cd(target, env);
+}
+
+static int exec_builtin_cd_export_unset(char **args, t_env *env)
 {
     char *equal;
-    
-    if (!args || !args[0])
-        return (0);
+    char *target;
+
     if (!strcmp(args[0], "cd"))
-        ft_cd(args[1] ? args[1] : getenv("HOME"));
-    else if (!strcmp(args[0], "echo"))
-        ft_echo(args);
-    else if (!strcmp(args[0], "pwd"))
-        ft_pwd();
-    else if (!strcmp(args[0], "env"))
-        ft_env(env);
+    {
+        handle_cd(args, env);
+        return (1);
+    }
     else if (!strcmp(args[0], "export") && args[1])
     {
         equal = strchr(args[1], '=');
@@ -50,9 +57,24 @@ int exec_builtin(char **args, t_env *env)
     }
     else if (!strcmp(args[0], "unset") && args[1])
         ft_unset(env, args[1]);
+    else
+        return (0);
+    return (1);
+}
+
+int exec_builtin(char **args, t_env *env)
+{
+    if (!args || !args[0])
+        return (0);
+    if (!strcmp(args[0], "echo"))
+        ft_echo(args);
+    else if (!strcmp(args[0], "pwd"))
+        ft_pwd();
+    else if (!strcmp(args[0], "env"))
+        ft_env(env);
     else if (!strcmp(args[0], "exit"))
         exit(0);
     else
-        return (0);
+        return (exec_builtin_cd_export_unset(args, env));
     return (1);
 }
