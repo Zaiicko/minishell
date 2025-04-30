703ec7b1c4e871a9619ddcddf8914b8e7df51850
Mehdi <zaiicko7@proton.me>
2025-04-30 20:22:33 +0200

    Fix : few occurences of same quotes pair

--- src/expander/expander_var.c
+++ src/expander/expander_var.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 18:07:02 by zaiicko           #+#    #+#             */
-/*   Updated: 2025/04/29 23:28:30 by zaiicko          ###   ########.fr       */
+/*   Updated: 2025/04/30 19:51:37 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,32 +35,41 @@ int	calculate_expanded_length(t_data *data, char *str)
 {
 	int	index;
 	int	result_len;
-	int	in_squotes;
-	int	in_dquotes;
+	t_expander	exp;
 
 	if (!str)
 		return (0);
 	index = 0;
 	result_len = 0;
-	in_squotes = 0;
-	in_dquotes = 0;
+	exp.in_squotes = 0;
+	exp.in_dquotes = 0;
+
 	while (str[index])
 	{
-		if (str[index] == '\'' && !in_dquotes)
-			in_squotes = !in_squotes;
-		else if (str[index] == '\"' && !in_squotes)
-			in_dquotes = !in_dquotes;
-		else if (str[index] == '$' && !in_squotes && str[index + 1]
-			&& ((str[index + 1] != '\"' && str[index + 1] != '\'')
-				|| in_dquotes)
+		if (str[index] == '\'' && !exp.in_dquotes)
+		{
+			exp.in_squotes = !exp.in_squotes;
+			result_len++;
+		}
+		else if (str[index] == '\"' && !exp.in_squotes)
+		{
+			exp.in_dquotes = !exp.in_dquotes;
+			result_len++;
+		}
+		else if (str[index] == '$' && !exp.in_squotes
+			&& str[index + 1] && ((str[index + 1] != '\"'
+					&& str[index + 1] != '\'') || exp.in_dquotes)
 			&& (ft_isalnum(str[index + 1]) || str[index + 1] == '_'
 				|| str[index + 1] == '?'))
+		{
+			exp.read_index = index;
 			process_var_len(data, str, &index, &result_len);
+		}
 		else
 			result_len++;
 		index++;
 	}
-	return (result_len + 2);
+	return (result_len + 1);
 }
 
 int	is_expandable_var(char *str, t_expander *exp)
