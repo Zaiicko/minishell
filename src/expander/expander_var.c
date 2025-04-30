dad2f66cc2e33c7765762d67205b702032c7f2f4
Mehdi <zaiicko7@proton.me>
2025-04-30 21:28:01 +0200

    Fix : leaks when extract_var_name & get_env_value fail

--- src/expander/expander_var.c
+++ src/expander/expander_var.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 18:07:02 by zaiicko           #+#    #+#             */
-/*   Updated: 2025/04/30 19:51:37 by zaiicko          ###   ########.fr       */
+/*   Updated: 2025/04/30 21:25:57 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,17 @@ static void	process_var_len(t_data *data, char *str, int *index_ptr,
 	temp_idx = *index_ptr;
 	var_name = extract_var_name(str, &temp_idx);
 	if (!var_name)
-		return ;
+		free_all_and_exit_perror(data,
+			"Error\n Extraction of env var_name failed\n");
 	var_value = get_env_value(data->env, var_name);
 	if (var_value)
 		*length_ptr += ft_strlen(var_value);
+	else
+	{
+		free(var_name);
+		free_all_and_exit_perror(data,
+			"Error\n Extraction of env_value failed\n");
+	}
 	*index_ptr = temp_idx - 1;
 	free(var_name);
 	free(var_value);
