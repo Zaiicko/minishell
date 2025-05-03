6772beefb500de4e7d82702a1ee7d931474fdb66
Mehdi <zaiicko7@proton.me>
2025-05-03 02:37:21 +0200

    Fix : few args in env init

--- src/init_data.c
+++ src/init_data.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 14:09:02 by zaiicko           #+#    #+#             */
-/*   Updated: 2025/04/27 20:53:21 by zaiicko          ###   ########.fr       */
+/*   Updated: 2025/05/03 02:36:47 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	init_data(t_data *data, char **env)
 	data->input = NULL;
 	data->tokens = NULL;
 	data->ast = NULL;
-	data->env = init_env(env);
+	data->env = init_env(env, data);
 	if (!data->env)
 		free_all_and_exit_perror(data, "Error\n Env init failed\n");
 }
